dae4beb5c54e196911836250a9398096d64fc7ad
Kwan Ling <m.kwanling@gmail.com>
2016-04-25 22:12:04 -0400

    add navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {NavbarComponent} from './navbar.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<navbar></navbar>',
+    directives: [NavbarComponent] 
 })
 export class AppComponent { }
